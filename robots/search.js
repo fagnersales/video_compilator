063f3c8b49bac2dd4a29042db8c0c47f032ac45f
Fagner Sales <fs.fagnersales@gmail.com>
2020-11-19 09:35:39 -0300

    feat: add log

--- robots/search.js
+++ robots/search.js
@@ -1,40 +1,44 @@
 const superagent = require('superagent')
 
-const { writeFileSync } = require('fs')
-const { post } = require('superagent')
-
 async function robot(state) {
+    console.log(`> SearchRobot: Started`)
+
     await fetchRedditPage(state)
 
     async function fetchRedditPage(state) {
 
-        const rawFetchedRedditData = await fetchReddit()
+        const rawFetchedRedditData = await fetchRedditData()
         const transformedRedditData = transformRedditData(rawFetchedRedditData)
         state.redditData = transformedRedditData
 
-
-        // const url = (data.data.children[0].data['media']['reddit_video']['scrubber_media_url'])
-
-        // superagent.get(url)
-        // .then(data => writeFileSync(url.split('/').reverse()[0], data.body))
-
-        async function fetchReddit() {
+        async function fetchRedditData() {
+            console.log(`> SearchRobot: Fetching Reddit Page Data`)
 
             const BaseURL = 'https://www.reddit.com'
 
             const URLToFetch = `${BaseURL}/r/${state.searchRedditPage}/hot.json`
 
-            const data = await superagent.get(URLToFetch)
-                .set('accept', 'json')
+            const data = await superagent.get(URLToFetch).set('accept', 'json')
+
+            data ? console.log(`> SearchRobot: Fetched data from: ${URLToFetch}`) : console.log(`> SearchRobot: No such data on: ${URLToFetch}`)
+
             return JSON.parse(data.text || '{}')
         }
 
         function transformRedditData(rawFetchedData) {
+            console.log(`> SearchRobot: Transforming Reddit Data`)
+
             const posts = rawFetchedData['data']['children']
 
+            console.log(`> SearchRobot: Filtering ${posts.length} posts`)
             const filteredPosts = filterPosts(posts)
+            console.log(`> SearchRobot: ${filteredPosts.length} posts passed the tests`)
+            console.log(`> SearchRobot: Sorting posts by upvotes`)            
             const sorteredPosts = sortPosts(filteredPosts)
+            console.log(`> SearchRobot: Pushing only important properties from posts`)
             const transformedData = getImportantRedditPostProperties(sorteredPosts)
+            console.log(`> SearchRobot: All the data has been trasnformed`)            
+            console.log(`> SearchRobot: Getting video URLs`)            
             const dataWithVideoURLs = getVideoURLs(transformedData)
             return dataWithVideoURLs
 
@@ -133,7 +137,6 @@ async function robot(state) {
                 })
             }
         }
-
     }
 }
 
